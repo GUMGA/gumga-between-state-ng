2497e17ba0e2cb78db06a4d5ea4cdb25c6038eca
Mateus Miranda <info.mateusmiranda@gmail.com>
2017-05-24 17:52:54 -0300

    criado metodo init controller

--- src/between-state.js
+++ src/between-state.js
@@ -1,5 +1,6 @@
-const BetweenState = () => {
+const BetweenState = ($timeout) => {
   return {
+    initController: initController,
     setPage: setPage,
     getPage: getPage,
     setSearch: setSearch,
@@ -11,7 +12,7 @@ const BetweenState = () => {
     this.page = page
   }
   function getPage() {
-    return this.page
+    return this.page || 1;
   }
   function setSearch(data) {
     this.search = data
@@ -26,7 +27,28 @@ const BetweenState = () => {
     return this.advancedSearch
   }
 
+  function initController(scope, container, pageModel = 'page'){
+    $timeout(()=>{
+      if(this.getSearch()){
+        scope[container].methods.search(this.getSearch().field, this.getSearch().param, scope[container].pageSize, this.getPage());
+      }else if(this.getAdvancedSearch()){
+        scope[container].methods.advancedSearch(this.getAdvancedSearch(), this.getPage());
+      }else if(this.getPage()){
+        scope[container].methods.get(this.getPage());
+      }
+      scope[pageModel] = this.getPage();
+      return {
+        page: this.getPage(),
+        search: this.getSearch(),
+        advancedSearch: this.getAdvancedSearch(),
+      };
+    })
+  }
+
 }
+
+BetweenState.$inject = ['$timeout'];
+
 angular
   .module('gumga.betweenstate', [])
   .service('BetweenState', BetweenState)
